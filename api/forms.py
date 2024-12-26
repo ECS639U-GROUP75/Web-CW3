776dd55bd4dc7f2ea39ef3409305eab6ae2c49e5
maqaro <basilnasr@outlook.com>
2024-12-26 03:32:25 +0000

    idek made some changes to the form

--- api/forms.py
+++ api/forms.py
@@ -3,6 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class UserForm(UserCreationForm):
+    date_of_birth = forms.DateField(
+        widget=forms.DateInput(attrs={'type': 'date'}),
+        help_text='Enter date in YYYY-MM-DD format'
+    )
+    
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'date_of_birth', 'password1', 'password2', 'Hobbies']
--- api/views.py
+++ api/views.py
@@ -6,24 +6,25 @@ from .forms import UserForm
 def main_spa(request: HttpRequest) -> HttpResponse:
     return render(request, 'api/spa/index.html', {})
 
-def register(request: HttpRequest) -> HttpResponse:
+def register_view(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             login(request, authenticate(
                 username=form.cleaned_data['username'],
                 password=form.cleaned_data['password1']
             ))
-        else:
-            form = UserForm()
+            return redirect('main_spa')
+    else:
+        form = UserForm()
     return render(request, 'api/spa/register.html', {'form': form})
 
-def login(request: HttpRequest) -> HttpResponse:
+def login_view(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(username=username, password=password)
+        user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             return redirect('main_spa')
