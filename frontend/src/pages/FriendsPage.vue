2c3b2b5c1ba55c8779eaf818056929fd0b145f8b
a-moh0 <130454520+a-moh0@users.noreply.github.com>
2025-01-14 23:43:29 +0000

    Update FriendsPage.vue

--- frontend/src/pages/FriendsPage.vue
+++ frontend/src/pages/FriendsPage.vue
@@ -1,6 +1,37 @@
 <template>
-    <div class="h1">
-      {{ title }}
+    <div>
+      <h1>Friend Requests</h1>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>From</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="request in friendRequests" :key="request.id">
+            <td>{{ request.user__username }}</td>
+            <td>
+              <button @click="acceptRequest(request.id)">Accept</button>
+              <button @click="rejectRequest(request.id)">Reject</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+  
+      <h2>Current Friends</h2>
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Friend</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="friend in currentFriends" :key="friend.friend__username">
+            <td>{{ friend.friend__username }}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </template>
   
@@ -18,4 +49,3 @@
   
   <style scoped>
   </style>
-  
